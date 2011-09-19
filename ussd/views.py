f1fe5df2fad975983f29ab9f25f816f780a153fd
David McCann <david.a.mccann@gmail.com>
2011-09-19 17:11:23 +0300

    wrapping get in the new form

--- ussd/views.py
+++ ussd/views.py
@@ -1,4 +1,8 @@
+from .forms import YoForm
 from django.http import HttpResponse
 
 def ussd(req):
+    form = YoForm(req.GET)
+    if form.is_valid():
+        pass
     return HttpResponse(content='responseString=Hello%2C+World%21&action=end', status=200)
