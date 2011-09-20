c6374dc1e6332245c0475bfb490a560ae854b8a9
David McCann <david.a.mccann@gmail.com>
2011-09-20 12:45:06 +0300

    updates to model, minor cleanup

--- ussd/models.py
+++ ussd/models.py
@@ -1,15 +1,17 @@
 from django.db import models
+from mptt.models import MPTTModel
 from rapidsms.models import Contact, Connection
+from rapidsms_xforms.models import XForm
 import mptt
 
 class USSDSession(models.Model):
     """Model to hold session information"""
 
     #a unique session identifier which is maintained for an entire USSD session/transaction
-    transaction_id  = models.CharField(max_length=100)
+    transaction_id = models.CharField(max_length=100)
 
     #The telephone number of the subscriber interacting with the USSD code
-	# match this with 'msisdn'
+    # match this with 'msisdn'
     connection = models.ForeignKey(Connection)
 
     #The number that the subscriber dialed.
@@ -18,21 +20,21 @@ class USSDSession(models.Model):
 
     #The information that the subscriber inputs to the system.
     ussd_request_string = models.CharField(max_length=100)
-	
-	#create XForm models and preferably fk to that.
-	xform_step = models.IntegerField()
 
-class MenuItem(models.Model):
-	parent = models.ForeignKey('self',null=True,blank=True,related_name='children')
-	label = models.CharField(max_length=50)
-	#xform_
-	#not sure of what type order should be associated to
-	#TODO fk order to something similar to ScriptStep orders
-	order = models.IntegerField()
+    current_menu_item = models.ForeignKey('MenuItem', null=True)
+    current_xform = models.ForeignKey(XForm, null=True)
+
+    #create XForm models and preferably fk to that.
+    xform_step = models.IntegerField()
 
 
-# register with mptt
-# when properly implemented, calls to 
-# menu Item placeholders is made heirarchical by order
-mptt.register(MenuItem,order_insertion_by=['order'])	
-	
+class MenuItem(MPTTModel):
+    parent = models.ForeignKey('self', null=True, blank=True, related_name='children')
+    label = models.CharField(max_length=50)
+    xform = models.ForeignKey(XForm, null=True)
+    #not sure of what type order should be associated to
+    #TODO fk order to something similar to ScriptStep orders
+    order = models.IntegerField()
+
+
+
