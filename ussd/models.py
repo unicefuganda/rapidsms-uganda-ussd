5e6e52632b08703d36f843cab9207100d58c57f2
David McCann <david.a.mccann@gmail.com>
2011-09-20 14:22:59 +0300

    added view logic

--- ussd/models.py
+++ ussd/models.py
@@ -1,8 +1,18 @@
 from django.db import models
+from django.forms import ValidationError
 from mptt.models import MPTTModel
 from rapidsms.models import Contact, Connection
-from rapidsms_xforms.models import XForm
+from rapidsms_xforms.models import XForm, XFormSubmission
 import mptt
+
+class MenuItem(MPTTModel):
+    parent = models.ForeignKey('self', null=True, blank=True, related_name='children')
+    label = models.CharField(max_length=50)
+    xform = models.ForeignKey(XForm, null=True)
+    #not sure of what type order should be associated to
+    #TODO fk order to something similar to ScriptStep orders
+    order = models.IntegerField()
+
 
 class USSDSession(models.Model):
     """Model to hold session information"""
@@ -19,18 +29,51 @@ class USSDSession(models.Model):
 
     current_menu_item = models.ForeignKey('MenuItem', null=True)
     current_xform = models.ForeignKey(XForm, null=True)
+    submission = models.ForeignKey(XFormSubmission, null=True)
 
     #create XForm models and preferably fk to that.
     xform_step = models.IntegerField()
 
+    def error_case(self):
+        return not (self.current_menu_item or self.current_xform) or \
+            (self.current_menu_item and self.current_menu_item.get_chidren.count() == 0)
 
-class MenuItem(MPTTModel):
-    parent = models.ForeignKey('self', null=True, blank=True, related_name='children')
-    label = models.CharField(max_length=50)
-    xform = models.ForeignKey(XForm, null=True)
-    #not sure of what type order should be associated to
-    #TODO fk order to something similar to ScriptStep orders
-    order = models.IntegerField()
+    def get_menu_text(self):
+        return "\n".join(["%d: %s" % (i.order, i.label) for i in self.current_menu_item.get_children().order_by(order)])
 
+    def advance_menu_progress(self, order):
+        try:
+            next_menu_item = self.current_menu_item.get_children().get(order=order)
+            if next_menu_item.get_children.count() == 0 and next_menu_item.xform:
+                self.current_menu_item = None
+                self.current_xform = next_menu_item.xform
+            else:
+                self.current_menu_item = next_menu_item
+            self.save()
+        except MenuItem.DoesNotExist:
+            pass
 
+    def process_xform_response(self, request_string):
+        response_content = ''
+        try:
+            if request_string and self.current_xform.fields.filter(order=self.xform_step).count():
+                try:
+                    field = self.current_xform.fields.get(order=self.xform_step)
+                    val = field.clean_submission(request_string)
+                    self.submission.values.create(attribute=field, value=val, entity=self.submission)
+                    self.xform_step = self.current_xform.fields.filter(order__gt=self.xform_step).order_by('order')[0].order
+                    self.save()
+                except ValidationError, e:
+                    response_content += "\n".join(e.messages)
 
+            else:
+                # create new submission object
+                self.xform_step = self.current_xform.fields.order_by('order')[0].order
+                self.submission = XFormSubmission.objects.create(xform=self.current_xform, \
+                                                                    has_errors=True)
+            response_content += self.current_xform.fields.get(order=self.xform_step).question
+            action = 'request'
+        except IndexError:
+            response_content = 'Your session has ended. Thank you.'
+            action = 'end'
+        return response_content, action
--- ussd/views.py
+++ ussd/views.py
@@ -1,19 +1,43 @@
 from .forms import YoForm
+from .models import MenuItem
+from django.forms import ValidationError
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from rapidsms_xforms.models import XFormSubmission
+import urllib
 
 def ussd(req, input_form=YoForm, request_method='GET', output_template='ussd/yo.txt'):
+    form = None
     if request_method == 'GET' and req.GET:
         form = input_form(req.GET)
     elif request_method == 'POST' and req.POST:
         form = input_form(req.POST)
-    else:
-        return HttpResponse(status=404)
-    if form.is_valid():
-        pass
 
-    return render_to_response(output_template, {
-        'response_content':'Hello%2C+World%21',
-        'action':'end',
-    }, context_instance=RequestContext(req))
+    if form and form.is_valid():
+        session = form.cleaned_data['transactionId']
+        request_string = form.cleaned_data['ussdRequestString']
+        response_content = ''
+        if session.error_case():
+            response_content = 'Your session has ended. Thank you.'
+            action = 'end'
+        elif session.current_menu_item:
+            if request_string:
+                try:
+                    order = int(request_string)
+                    session.advance_menu_progress(order)
+                    pass
+                except ValueError:
+                    response_content += "Invalid Menu Option.\n"
+            response_content += session.get_menu_text()
+            action = 'request'
+
+        if session.current_xform:
+            response_content, action = session.process_xform_response(request_string)
+        return render_to_response(output_template, {
+            'response_content':urllib.quote(response_content),
+            'action':action,
+        }, context_instance=RequestContext(req))
+
+    return HttpResponse(status=404)
+
