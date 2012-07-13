591a7331fa2d914658efdcab55f259c63d1bba63
Alfred Assey M <asseym@gmail.com>
2012-07-13 09:28:10 +0300

    integrated mptt admin templates and custom mptt admin

--- ussd/admin.py
+++ ussd/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
-from .models import Menu, Question, Field
+from .models import Screen, Menu, Question, Field
+from mptt.admin import MPTTModelAdmin
 
-admin.site.register(Question)
-admin.site.register(Field)
-admin.site.register(Menu)
+admin.site.register(Screen, MPTTModelAdmin)
+admin.site.register(Question, MPTTModelAdmin)
+admin.site.register(Field, MPTTModelAdmin)
+admin.site.register(Menu, MPTTModelAdmin)
 
 
 
